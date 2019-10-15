69533f9241330fe7801bc78982a3dcdf49ebb6c2
Jonathan Keane <jkeane@gmail.com>
2019-10-15 00:36:44 -0500

    test

--- mariadb-travis-macos.sh
+++ mariadb-travis-macos.sh
@@ -1,6 +1,7 @@
 rm -f /etc/my.cnf
 brew install mariadb
-brew services start mariadb
+brew services start mariadbd
+mkdir /usr/local/etc/my.cnf.d
 sleep 5
 mysql -uroot -e "CREATE DATABASE nycflights;"
 mysql -uroot -e "CREATE USER IF NOT EXISTS travis@'%'; GRANT ALL ON *.* TO travis@'%'; FLUSH PRIVILEGES;"
