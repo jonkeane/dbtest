abd648ad6cdb6d1902a118e83fd2e92b3de8b9e9
Jonathan Keane <jkeane@gmail.com>
2019-10-14 22:00:49 -0500

    different macos start

--- postgres-macos.sh
+++ postgres-macos.sh
@@ -1,6 +1,8 @@
-brew services start postgresql
+rm -rf /usr/local/var/postgres
+initdb /usr/local/var/postgres
+pg_ctl -D /usr/local/var/postgres start
 
-createuser -s root
-psql -U root -c "CREATE ROLE username WITH LOGIN PASSWORD '6c9FT%Kj'"
+createuser -s postgres
+psql -U postgres -c "CREATE ROLE travis WITH LOGIN PASSWORD '6c9FT%Kj'"
 export PGPASSWORD=6c9FT%Kj
 psql -U travis -c "CREATE DATABASE nycflights;"
