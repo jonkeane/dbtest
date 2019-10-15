ee4c9ba0677e18f629ff81189e7ef5d38be4c583
Jonathan Keane <jkeane@gmail.com>
2019-10-15 01:35:21 -0500

    sudo again

--- mariadb-travis-macos.sh
+++ mariadb-travis-macos.sh
@@ -3,5 +3,5 @@ brew install mariadb
 brew services start mariadb
 mkdir /usr/local/etc/my.cnf.d
 sleep 5
-mysql -u root -e "CREATE DATABASE nycflights;"
-mysql -u root -e "CREATE USER IF NOT EXISTS travis@'%'; GRANT ALL ON *.* TO travis@'%'; FLUSH PRIVILEGES;"
+sudo mysql -u root -e "CREATE DATABASE nycflights;"
+sudo mysql -u root -e "CREATE USER IF NOT EXISTS 'travis'@'%'; GRANT ALL ON *.* TO 'travis'@'%'; FLUSH PRIVILEGES;"
