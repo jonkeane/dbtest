0c654ed1459310dc5f2e7468b4ddecef35b7445a
Jonathan Keane <jkeane@gmail.com>
2019-10-14 22:31:47 -0500

    sigh

--- postgres-macos.sh
+++ postgres-macos.sh
@@ -1,4 +1,4 @@
 rm -rf /usr/local/var/postgres
 initdb /usr/local/var/postgres
 pg_ctl -D /usr/local/var/postgres start
-POSTGRES_USER=travis psql -c "CREATE DATABASE nycflights;"
+psql -c "CREATE DATABASE nycflights;"
